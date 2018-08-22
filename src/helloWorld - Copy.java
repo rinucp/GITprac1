7bde5334d7662c29262338bd38d90d8db172827e
DESKTOP-DAUN7QD\Kenneth <kennethgoh96.989@gmail.com>
2018-08-22 18:58:02 +0800

    changed git ignore file and changes to 'Copy' ver.

--- src/helloWorld - Copy.java
+++ src/helloWorld - Copy.java
@@ -1,7 +1,7 @@
 public class helloWorld{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Made changes in the copy file");
     }
 
 }
